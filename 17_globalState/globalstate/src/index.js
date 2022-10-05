99c39ccbc9378e3fc18d579fecf91240cb4bdb5f
muhammhafizh <hafizhsusanto9@gmail.com>
2022-10-05 17:28:36 +0700

    Redux persist added

--- 17_globalState/globalstate/src/index.js
+++ 17_globalState/globalstate/src/index.js
@@ -8,7 +8,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-let persistor = persistStore(store) 
+const persistor = persistStore(store) 
 
 root.render(
   <Provider store={store}>
